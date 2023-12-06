775d2a359ab04bed47c64ac8495700f357386248
조하은 <108499120+hani0903@users.noreply.github.com>
2023-12-06 22:39:27 +0900

    Feat: 농산물의 가격정보 갖고오기
    공공 데이터에서 가격 정보를 갖고 오는 코드를 작성함

--- getPrice.py
+++ getPrice.py
@@ -5,7 +5,11 @@ import sqlite3
 from user_DB import *
 
 
-def getPrice(p_cert_key, p_cert_id, response_format):
+def getPrice(crops_name):
+
+    p_cert_key = "8b73d53c-3f47-48bb-a8fb-5541b85d3ace"
+    p_cert_id = "jhe93"
+    response_format = "xml"
     
     base_url = "http://www.kamis.or.kr/service/price/xml.do"
 
@@ -37,37 +41,20 @@ def getPrice(p_cert_key, p_cert_id, response_format):
 
         for item in items:
             item_name = item.get('item_name', 'N/A')
+            unit = item.get('unit', 'N/A')
             todayPrice = item.get('dpr1', 'N/A')
 
+            item_name = item_name.split("/")[0]
+
             # 날짜와 위치가 중복되지 않으면 출력하고 중복을 추가
-            print(f"품목 : {item_name}   오늘의 도매가 : {todayPrice}")
+            # print(f"품목 : {item_name}   오늘의 도매가 : {todayPrice}")
+
+            if(item_name == crops_name):
+                return todayPrice, unit
+            
+        return "-","-"
             
     else:
         print(f'에러 코드 : {res.status_code}')
 
-if __name__ == '__main__':
-    p_cert_key = "8b73d53c-3f47-48bb-a8fb-5541b85d3ace"
-    p_cert_id = "jhe93"
-
-    create_table()
-    user_name = input("사용자의 이름을 입력하세요: ")
-    user_location = get_user_location(user_name)
-
-    if user_location:
-        print(f"{user_name}님의 위치는 {user_location}입니다.")
-        
-    else:
-        print(f"{user_name}님의 정보를 찾을 수 없습니다.")
-        save_user(user_name, input(f"{user_name}님의 위치를 입력하세요: "))
-        user_location = get_user_location(user_name)
-        print(f"{user_name}님의 위치는 {user_location}입니다.")
-
-    crops_name = input("재배 중인 작물을 입력하세요: ")
-    save_crops(crops_name, "3월")
-    user_id = get_user_id(user_name)
-    crops_id = get_crops_id(crops_name)
-
-    print(user_id, crops_id)
-    save_user_crops(crops_id, user_id)
-
-    getPrice(p_cert_key, p_cert_id, "xml")
+    # 10주차
